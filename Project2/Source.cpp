767d85b039222c4e95a435db7a109a7091e32247
Seb Stuck <sebas7236@icloud.com>
2023-03-06 13:05:34 -0700

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int Numbers(int a, int b);
+int main() {
+	int input1;
+	int input2;
+	cout << "type two numbers" << endl;
+	cin >> input1 >> input2;
+	Numbers(input1, input2);
+	cout << endl;
+}
+
+int Numbers(int a, int b) {
+	//sum
+	cout << "the sum of " << a << " and " << b << " is " << a + b << endl;
+	//difference
+	cout << "the difference of " << a << " and " << b << " is " << a - b << endl;
+	//product
+	cout << "the product of " << a << " and " << b << " is " << a * b << endl;
+	//quotient
+	cout << "the quotient of " << a << " and " << b << " is " << a / b << endl;
+	//average
+	cout << "the average of " << a << " and " << b << " is " << (a + b) / 2 << endl;
+	return (a+b)/2;
+}
